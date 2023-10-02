54d8409c8ab18dd39b7129323a60028a75cad7bb
Alexandra Zhang Jiang <52464618+Ale-ZJ@users.noreply.github.com>
2023-10-01 22:36:19 -0700

    Code cleanup and delete .nfs files

--- src/checkers-cpp/StudentAI.h
+++ src/checkers-cpp/StudentAI.h
@@ -7,11 +7,6 @@
 #pragma once
 
 
-// struct MinimaxPair{
-//     int value; 
-//     Move move;
-// };
-
 
 class MCNode
 {
@@ -76,10 +71,8 @@ private:
     
 
 private: // member variables that we added
-//     const int MINIMAX_DEPTH = 3;
     const int MATURITY_THRESHOLD = 200;
-    const int NUMBER_OF_SIMULATIONS = 1100;
-
+    const int NUMBER_OF_SIMULATIONS = 1500;
 
 
 private: // Monte Carlo Tree Search algorithm
@@ -89,44 +82,6 @@ private: // Monte Carlo Tree Search algorithm
     // selection, expansion, rollout, and backpropagation 
     void simulateGames();
 
-//     // Selects the children node with the highest UCT value
-//     int select(int nodeIdx);
-
-//     // Increases the depth of the game tree by one level.
-//     // We expand when we reach a leaf node in the MCTree that has 
-//     // already been visited 
-//     int expand(int parentIdx, int turn);
-
-//     // Randomly plays a game until one player win.
-//     // returns 1 if the rollout results in a win for the selectedPlayer
-//     // or 0 if the player lost
-//     double rollout(int nodeIdx, int turn);
-
-//     // updates the w_i and s_i member variables from a given node 
-//     // PRE-CONDITION: check the player's turn to pass the correct win value
-//     void backpropagate(double score, int nodeIdx);
-
-//     // calculates the UCT value of the node at the given index.
-//     // UCT value = (w_i/s_i) + (MCTS_UCT * sqrt(log(s_p)/s_i))
-//     // The AI will select the move with the highest UCT value
-//     double calculateUCT(int nodeIdx);
-
-//     //MCNode makeNewMCNode(Board b, Move parentMove, int parentIdx);
-
-//     // given a board state and a player's turn, we calculate all the possible moves 
-//     // for the player and we add them to the tree. We also update the parentNode a needed
-//     void addMovesToTree(int parentIdx, int turn);
-
-// private: // minimax algorithm
-//     // returns the utility/heuristic value given the state of the game
-//     double evaluate(const Board& board, int selectedPlayer);
-
-    // // recursive algorithm to pick best move
-    // int minimax(Board board, int minimaxPlayer, int depth);
-
-    // //pair<int, Move> evalMin(int depth, Board board, int maxPlayer);
-    // int evalMax(Board board, int maxPlayer, int selectedPlayer, int depth, int alpha, int beta);
-    // int evalMin(Board board, int minPlayer, int selectedPlayer, int depth, int alpha, int beta);
 };
 
 #endif //STUDENTAI_H
